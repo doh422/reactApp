3c9e41e7cec820925fc2061b94a55194fb92b390
Daniel <doh422@icloud.com>
2020-03-30 19:39:29 -0700

    add content for players and contact components and create routing component

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Players from './players';
+import Contact from './contact';
 
+
+const routing = (
+	<Router>
+		<div>
+			<Route path="/" component={App} />
+			<Route path="/players" component={Players} />
+			<Route path="/contact" component={Contact} />
+		</div>
+	</Router>
+)
 
 ReactDOM.render(
-  <App />,
+  //<App />,
+  routing,
   document.getElementById('root')
 );
---
+++ client/src/players.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Players extends Component {
+	render() {
+		return <h1>Players</h1>
+	}
+}
+
+export default Players;
