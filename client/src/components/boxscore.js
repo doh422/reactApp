7980275945c93456e6e41a4b32c968fc1972d3a4
Daniel <doh422@icloud.com>
2020-04-15 19:06:06 -0700

    make boxscore table and pass in props to component

--- client/src/components/boxscore.js
+++ client/src/components/boxscore.js
@@ -2,8 +2,40 @@ import React, { Component } from 'react';
 
 class Boxscore extends Component {
     render() {
+        const { roadTeam, homeTeam, location, date, status } = this.props.location.state;
         return (
-            <h3>Box score component</h3>
+            <div>
+                <h3>{roadTeam + " @ " + homeTeam}</h3>
+                <p>{location + " - " + date}</p>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Player</th>
+                            <th>AB</th>
+                            <th>R</th>
+                            <th>H</th>
+                            <th>RBI</th>
+                            <th>BB</th>
+                            <th>K</th>
+                            <th>AVG</th>
+                            <th>OBP</th>
+                            <th>SLG</th>
+                        </tr>
+                        <tr>
+                            <td><input type="text" placeholder="Player" /></td>
+                            <td><input type="number" /></td>
+                            <td><input type="number" /></td>
+                            <td><input type="number" /></td>
+                            <td><input type="number" /></td>
+                            <td><input type="number" /></td>
+                            <td><input type="number" /></td>
+                            <td><input type="number" /></td>
+                            <td><input type="number" /></td>
+                            <td><input type="number" /></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
